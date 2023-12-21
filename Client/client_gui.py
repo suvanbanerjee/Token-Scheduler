8a566675be34a5fa687b486bc2f645d420f0b7d3
suvanbanerjee <banerjeesuvan@gmail.com>
2023-12-21 20:00:00 +0530

    partial implemention of send token to server

--- Client/client_gui.py
+++ Client/client_gui.py
@@ -1,4 +1,15 @@
 import PySimpleGUI as sg
+import socket
+
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+
+
+def NextToken(MESSAGE):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
+
 
 layout = [[sg.Column([[sg.Button('Next Token', size=(10, 2))]], justification='center')],
     [sg.Column([[sg.Button('Hold Token', size=(10, 2))]], justification='center')],
