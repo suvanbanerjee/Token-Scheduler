864c1af07f9e6835c179b5025653f84766e25a92
suvanbanerjee <banerjeesuvan@gmail.com>
2024-01-02 19:30:53 +0530

    fixed messy code

--- Client/client_generator.py
+++ Client/client_generator.py
@@ -1,19 +1,23 @@
 import PySimpleGUI as sg
-sg.theme('LightGrey1')
-count=1
 
-def GenToken(counter):
-    token=str(count)+counter
-    count+=1
+sg.theme('LightGrey1')
+count = 1
+
+def gen_token(counter):
+    global count
+    token = str(count) + counter
+    count += 1
     return token
 
 layout = [
-    [sg.Column([[sg.Button('Token A', size=(10, 2) , font="Times 50")]], justification='center')],
+    [sg.Column([[sg.Button('Token A', size=(10, 2), font="Times 50")]], justification='center')],
     [sg.Column([[sg.Button('Token C', size=(10, 2))]], justification='center')],
     [sg.Column([[sg.Button('Token C', size=(10, 2))]], justification='center')],
     [sg.Column([[sg.Button('Token D', size=(10, 2))]], justification='center')],
-    [sg.Column([],size=(10, 10))],
-    [sg.Column([[sg.Button('Close', size=(100, 20))]], justification='center')]]
+    [sg.Column([], size=(10, 10))],
+    [sg.Column([[sg.Button('Close', size=(100, 20))]], justification='center')]
+]
+
 window = sg.Window('Token Scheduler', layout, finalize=True, resizable=True)
 window.maximize()
 
@@ -25,4 +29,5 @@ while True:
         pass
     elif event == 'Put on Hold':
         pass
+
 window.close()
