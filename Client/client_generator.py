1ddbeab221cab598ac2bb4700c5e2624fc39975c
suvanbanerjee <banerjeesuvan@gmail.com>
2024-01-02 20:17:52 +0530

    completed gui of client_generator

--- Client/client_generator.py
+++ Client/client_generator.py
@@ -10,24 +10,31 @@ def gen_token(counter):
     return token
 
 layout = [
-    [sg.Column([[sg.Button('Token A', size=(10, 2), font="Times 50")]], justification='center')],
-    [sg.Column([[sg.Button('Token C', size=(10, 2))]], justification='center')],
-    [sg.Column([[sg.Button('Token C', size=(10, 2))]], justification='center')],
-    [sg.Column([[sg.Button('Token D', size=(10, 2))]], justification='center')],
-    [sg.Column([], size=(10, 10))],
-    [sg.Column([[sg.Button('Close', size=(100, 20))]], justification='center')]
+    [sg.Text('\nWelcome to Namma Bank\n', font='Helvetica 50 bold', justification='centre', expand_x=True)],
+    [sg.Text('Please seclect a Counter to get a token\n', font='Helvetica 20', justification='center', expand_x=True)],
+    [sg.Column([[sg.Button('A', size=(5, 3), font='Helvetica 30'),
+                sg.Button('B', size=(5, 3), font='Helvetica 30'),
+                sg.Button('C', size=(5, 3), font='Helvetica 30'),
+                sg.Button('D', size=(5, 3), font='Helvetica 30')]], justification='center')],
+    
 ]
 
 window = sg.Window('Token Scheduler', layout, finalize=True, resizable=True)
 window.maximize()
-
 while True:
     event, values = window.read()
-    if event == sg.WINDOW_CLOSED or event == 'Close':
+    if event == sg.WINDOW_CLOSED:
         break
-    elif event == 'Next Token':
-        pass
-    elif event == 'Put on Hold':
-        pass
-
+    elif event:
+        token=gen_token(event)
+        layout2=[
+            [sg.Text('\n  Your token is \n', font='Helvetica 20', justification='center', expand_x=True)],
+            [sg.Text(token, font='Helvetica 50 bold', justification='center', expand_x=True, text_color='#0079D3')],
+            [sg.Text('\n  Please wait for your turn \n', font='Helvetica 15', justification='center', expand_x=True)],
+            [sg.Column([[sg.Button('Close', size=(5, 2), font='Helvetica 12', button_color="black")]], justification='center')]
+        ]
+        token_window = sg.Window('Token', layout2, finalize=True, resizable=False)
+        event, values = token_window.read()
+        if event == sg.WINDOW_CLOSED or event == 'Close':
+            token_window.close()
 window.close()
