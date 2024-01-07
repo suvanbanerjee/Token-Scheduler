92edc4df9f514ca2e65ecd2c470b7383c5286a30
suvanbanerjee <banerjeesuvan@gmail.com>
2024-01-07 19:07:22 +0530

    added audio feedback to client_counter

--- Client/client_counter.py
+++ Client/client_counter.py
@@ -1,18 +1,19 @@
 import PySimpleGUI as sg
 import socket
+import pygame.mixer
+
+pygame.mixer.init()
+ding = pygame.mixer.Sound('ding.mp3')
 sg.theme('LightGrey1')
-
-
 
 TCP_IP = "127.0.0.1"
 TCP_PORT = 1234
-
-
 
 def NextToken():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     if sock.connect_ex((TCP_IP, TCP_PORT)) == 0:
         sock.sendto(bytes("next_token", "utf-8"), (TCP_IP, TCP_PORT))
+        ding.play()
     else:
         sg.popup("Server is not running")
 
@@ -20,6 +21,7 @@ def HoldToken():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     if sock.connect_ex((TCP_IP, TCP_PORT)) == 0:
         sock.sendto(bytes("hold_token", "utf-8"), (TCP_IP, TCP_PORT))
+        ding.play()
     else:
         sg.popup("Server is not running")
 
