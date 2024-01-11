8821efa6ba645431c10244b01ace215a75288d37
suvanbanerjee <banerjeesuvan@gmail.com>
2024-01-11 22:12:47 +0530

    added json dump

--- Sceduler and Server/scheduler.py
+++ Sceduler and Server/scheduler.py
@@ -1,3 +1,4 @@
+import json
 import socket
 global queue 
 TCP_IP = "192.168.83.25"
@@ -44,6 +45,9 @@ def fcs_scheduler():
             token = token.decode()
             insert_token(data)  
         print(queue)
+        fp=open("queue_state.json","w")
+        json.dump(queue,fp)
+        fp.close()
         client_socket.close()
     server_socket.close()
 
