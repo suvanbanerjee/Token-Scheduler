7b6504b74ba0040deaf2e0bd21a299b9a5ddbd48
suvanbanerjee <banerjeesuvan@gmail.com>
2024-01-19 20:26:29 +0530

    updated main.py

--- Sceduler and Server/main.py
+++ Sceduler and Server/main.py
@@ -1,17 +1,25 @@
-from pyscript import document
-import json
+from pyscript import display
+import socket
 
-with open("data.json","r") as file:
-    while True:
-        token_queue=json.load(file)
-        if len(token_queue)<4:
-            for _ in range(4-len(token_queue)):
-                token_queue.append(0) 
-        output_toke_1 = document.querySelector("#token_1")
-        output_toke_2 = document.querySelector("#token_2")
-        output_toke_3 = document.querySelector("#token_3")
-        output_toke_4 = document.querySelector("#token_4")
-        output_toke_1.innerText = token_queue[0]
-        output_toke_2.innerText = token_queue[1]
-        output_toke_3.innerText = token_queue[2]
-        output_toke_4.innerText = token_queue[3]
+TCP_IP = "127.0.0.1"
+TCP_PORT = 1234
+token_queue = []
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((TCP_IP, TCP_PORT))
+server_socket.listen(1)
+
+while True:
+    client_socket, address = server_socket.accept()
+    print("Connection from", address)
+    data = client_socket.recv(1024)
+    data = data.decode()
+    token_queue.append(data)
+    client_socket.close()
+    if len(token_queue)<4:
+        for _ in range(4-len(token_queue)):
+            token_queue.append(0) 
+    display(token_queue[0], target="token_1", append=False)
+    display(token_queue[1], target="token_2", append=False)
+    display(token_queue[2], target="token_3", append=False)
+    display(token_queue[3], target="token_4", append=False)
